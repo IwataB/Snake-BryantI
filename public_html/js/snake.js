f7a26ce19ef4fe0d5e35368c880dfebf99366f52
Bryant Iwata <pbomber07@gmail.com>
2014-10-06 10:43:52 -0700

    End of 10/6/14

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,7 +1,24 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var snake;
 
+var context;
+var screenHeight;
+var screenWidth;
 
+function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    
+}
