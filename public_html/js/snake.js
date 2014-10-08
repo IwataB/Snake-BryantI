dc1dd287194af2eb149f4c57f03e48773972a1f8
Bryant Iwata <pbomber07@gmail.com>
2014-10-08 09:20:53 -0700

    End of 10/8/14

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -7,7 +7,9 @@ var screenHeight;
 var screenWidth;
 
 gameInitialize();
+snakeInitialize();
 gameDraw();
+snakeDraw();
 
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
@@ -33,7 +35,29 @@ function snakeInitialize() {
     snake = [];
     snakeLength = 5;
     snakeSize = 20;
-    for(var index = 0; index < snakeLength; index++) {
-        
+    for(var index = snakeLength-1; index >= snakeLength; index--) {
+        snake.push( {
+            x: index,
+            y: 0,
+        }) ;
     }
+}
+
+function snakeDraw() {
+    for(var index = 0; index < snake.length; index++) {
+        context.fillStyle = "white";
+        context.fillRect(snake[index].x*snakeSize, snake[index].y*snakeSize, snakeSize, snakeSize) ;
+    }
+}
+
+function snakeUpdate() {
+    var snakeHeadX = snake[0].x;
+    var snakeHeadY = snake[0].y;
+    
+    snakeHeadX++;
+    
+    var snakeTail = snake.pop() ;
+    snakeTailX.x = snakeHeadX;
+    snakeTail.y = snakeHeadY;
+    snake.unshift(snakeTail);
 }
